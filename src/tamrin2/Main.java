bcbb79a9832bdf53bc714d39d29d624e9ea6a94a
mohsen <mohsen1993asgari@gmail.com>
2019-04-17 09:14:44 +0430

    Call MyLinkedList Methods

--- src/tamrin2/Main.java
+++ src/tamrin2/Main.java
@@ -6,9 +6,28 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         MyLinkedList<Integer> list = new MyLinkedList<>();
+        System.out.println(list.isNull());
         for (int i=0 ; i<10; i++){
             list.insert(random.nextInt(20)+5);
         }
-        
+        System.out.println(list.isNull());
+        System.out.println(list);
+        list.deleteByPosition(5);
+        System.out.println(list);
+        list.deleteByPosition(0);
+        System.out.println(list);
+
+        list.addNode(48,7);
+        System.out.println(list);
+
+        list.deleteByKey(48);
+        System.out.println(list);
+
+        list.search(8);
+
+        System.out.println(list.contain(10));
+        list.insert(99);
+        System.out.println(list.contain(99));
+
     }
 }
