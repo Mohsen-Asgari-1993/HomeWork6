78ada1164783fb395bd9ee92e02bd34ddfc12673
mohsen <mohsen1993asgari@gmail.com>
2019-04-17 08:44:07 +0430

    Adds initializeList Method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
-
+    private static void initializeList(ArrayList<Integer> list) {
+        Random random = new Random();
+        for (int i = 0; i < random.nextInt(16) + 5; i++) {
+            list.add(random.nextInt(201) - 100);
+        }
+    }
 }
