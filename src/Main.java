7071d74dfec2dd8a3d477f2730b7fdfc0d5724d8
mohsen <mohsen1993asgari@gmail.com>
2019-04-17 08:45:30 +0430

    Adds sort Method

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,13 @@ public class Main {
             list.add(random.nextInt(201) - 100);
         }
     }
+
+    private static void sort(ArrayList<Integer> list) {
+        for (int i = list.size() - 1; i >= 0; i -= 2) {
+            if (list.get(i) < list.get(i - 1)) {
+                list.remove(i);
+                list.remove(i - 1);
+            }
+        }
+    }
 }
