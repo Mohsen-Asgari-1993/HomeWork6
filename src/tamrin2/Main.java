7e15beb8986dea3bc8c128230da17a35edbafd97
mohsen <mohsen1993asgari@gmail.com>
2019-04-17 09:08:19 +0430

    Creates Instance Of Random And MyLinkedList Insert 10 Element into List

--- src/tamrin2/Main.java
+++ src/tamrin2/Main.java
@@ -1,7 +1,14 @@
 package tamrin2;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Random random = new Random();
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        for (int i=0 ; i<10; i++){
+            list.insert(random.nextInt(20)+5);
+        }
         
     }
 }
