b4f6e4dd87db2b1261ddd3c68a8f4af5f309bd1f
mohsen <mohsen1993asgari@gmail.com>
2019-04-17 08:53:34 +0430

    Adds Fields (size, MyNode) And Two Method(isNull(), size())

--- src/tamrin2/MyLinkedList.java
+++ src/tamrin2/MyLinkedList.java
@@ -2,6 +2,17 @@ package tamrin2;
 
 public class MyLinkedList<E> {
 
+    private static int size = 0;
+    private MyNode first;
+
+    public boolean isNull() {
+        return (size == 0);
+    }
+
+    public int size() {
+        return size;
+    }
+
     private class MyNode {
         E item;
         MyNode next;
